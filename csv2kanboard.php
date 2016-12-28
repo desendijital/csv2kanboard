8be19ab883a30f3830b15d02eec0ea10ba3c7065
Sencar TOSUN <siberaktif@gmail.com>
2016-12-28 17:09:37 +0300

    Update
    Turkish message

--- csv2kanboard.php
+++ csv2kanboard.php
@@ -25,20 +25,46 @@ if ($handle) {
 	}
 } else {
 	printf ( "\n  Using webhook URL from defined in script...\n" );
+/*
+ * A simple PHP script to parse CSV file and create tasks in Kanboard (http://www.kanboard.net).
+ * The CSV should be structured this way:
+ * - First line is assumed as header and skipped.
+ * - Columns must be in this order: project_id, title, description, color_id, owner_id, column_id
+ * - Mandatory field:
+ * - project_id: If not provided, row will be skipped.
+ * - title: If not provided, row will be skipped.
+ */
+
+$handle = fopen ( ".csv2kanboard", "r" );
+if ($handle) {
+	if (($line = fgets ( $handle )) !== false) {
+		$webhookurl = trim ( $line );
+		printf ( "\n  Tr: Yerel olarak depolanan .csv2kanboard dosyasındaki webkanca-webhook URL'sini kullanın ... </br>   		 En: Using webhook URL from locally stored .csv2kanboard file...\n\n" );
+		fclose ( $handle );
+	}
+} else {
+	printf ( "\n\n  Tr: Komut dosyasında tanımlanan webkanca-webhook URL'sini kullanın ... \n </br>
+					En: Using webhook URL from defined in script...\n\n" );
 }
 
 $filename = $argv [1];
 
 if (empty ( $filename )) {
-	printf ( "\n  SYNTAX: csv2kanboard.php <filename>. You need to pass a comma delimited filename as argument. For more information, see https://github.com/ashbike/csv2kanboard/blob/master/README.md\n\n" );
+	printf ( "</br>\n\n  Tr: YAZIM: csv2kanboard.php <dosyaadı>. Argüman olarak virgül ile ayrılmış bir csv dosya adı belirtmelisiniz. Daha fazla bilgi için bkz.: https://github.com/ashbike/csv2kanboard/blob/master/README.md\n\n </br>
+					En: SYNTAX: csv2kanboard.php <filename>. You need to pass a comma delimited filename as argument. For more information, see https://github.com/ashbike/csv2kanboard/blob/master/README.md\n\n" );
 	exit ( 1 );
 }
 
-printf ( "\n  Opening file %s...\n", $filename );
+printf ( "\n\n  Tr: Dosya açılıyor %s...\n </br>
+				En: Opening file %s...\n", $filename );
 $file_handle = fopen ( $filename, "r" );
 
 if (! $file_handle) {
-	printf ( "\n\n  Cound not open file %s. Exiting...\n\n", $filename );
+	printf ( "\n\n\n  Tr: %s. dosyası açılamadı </br>
+	\n 				  En:Cound not open file %s.
+	
+	\n\n 			  Tr: Çıkış 
+	\n 			  	  En:Exiting...\n\n", $filename );
 	exit ( 1 );
 }
 
@@ -57,7 +83,8 @@ while ( ! feof ( $file_handle ) ) {
 
 	$project_id = trim ( $row [0] );
 	$title = trim ( $row [1] );
-	printf ( "  Processing row [%'.4u]...    ", $rownum );
+	printf ( "  \n\n Tr: İşlem Sırası </br>
+	 			  	 En:Processing row [%'.4u]...    ", $rownum );
 	
 	if (! empty ( $project_id ) && ! empty ( $title )) {
 		$url = $webhookurl;
@@ -79,17 +106,20 @@ while ( ! feof ( $file_handle ) ) {
 		
 		$httpcode = curl_getinfo ( $curl, CURLINFO_HTTP_CODE );
 		if ($httpcode == '401') {
-			printf ( "!!! Your webhook URL is not correct or token is no longer valid. Please check from browser. For more information, see https://github.com/ashbike/csv2kanboard/blob/master/README.md\n\n" );
+			printf ( "\n\n Tr: !!! Web-kanca webhook URL'niz doğru değil veya token-simge artık geçerli değil. Lütfen tarayıcıdan kontrol edin. Daha fazla bilgi için bkz. https://github.com/ashbike/csv2kanboard/blob/master/README.md\n\n </br>
+			En: !!! Your webhook URL is not correct or token is no longer valid. Please check from browser. For more information, see https://github.com/ashbike/csv2kanboard/blob/master/README.md\n\n" );
 			exit ( 2 );
 		}
 		printf ( "\n" );
 	} else {
-		printf ( "SKIPPED. Missing project_id or title.\n" );
+		printf ( "\n\n Tr: Atladı. Project_id veya başlık eksik.  
+		</br></br>	   En: SKIPPED. Missing project_id or title.\n" );
 	}
 }
 
 curl_close ( $curl );
 fclose ( $file_handle );
 
-printf ( "  Finished. \n\n" );
+printf ( "\n\n  Tr: Bitti 
+	</br> 		En:Finished. \n\n" );
 ?>
